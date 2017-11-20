f76dea83baea209a086e34ed0893666e64bd6149
Unknown <bart.vermeylen@student.ap.be>
2017-11-20 16:46:28 +0100

    EventBinding(2)

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Console } from '@angular/core/src/console';
 
 @Component({
   selector: 'app-calc',
   templateUrl: './calc.component.html'
 })
 export class CalcComponent {
+  counter: number = 0;
+
+  Increment():void{
+    this.counter++;
+  }
 }
